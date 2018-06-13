bedab88f7fe6e57915277e47c0a54d25eb573063
aditya <aditya.ghag@wohlig.com>
2018-06-13 13:33:20 +0530

    changes

--- api/controllers/PlansController.js
+++ api/controllers/PlansController.js
@@ -1,3 +1,17 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+
+    searchByFilter: function (req, res) {
+        if (req.body) {
+            Plans.searchByFilter(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: {
+                    message: "Invalid Request"
+                }
+            })
+        }
+    },
+};
 module.exports = _.assign(module.exports, controller);
--- api/services/Plans.js
+++ api/services/Plans.js
@@ -3,7 +3,7 @@ var schema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "User"
     },
-    script:String,
+    script: String,
     depositeScript1Transaction: {
         type: Schema.Types.ObjectId,
         ref: "Transaction"
@@ -24,11 +24,11 @@ var schema = new Schema({
         type: String,
         enum: ["Pending", "Terminated"]
     },
-    statusLog:[{
-        status:String,
-        timeStamp:Date
+    statusLog: [{
+        status: String,
+        timeStamp: Date
     }],
-    lastCheckTime:Date
+    lastCheckTime: Date
 });
 
 schema.plugin(deepPopulate, {});
@@ -37,5 +37,51 @@ schema.plugin(timestamps);
 module.exports = mongoose.model('Plans', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
-var model = {};
+var model = {
+
+    searchByFilter: function (data, callback) {
+        var pipeline = [
+            // Stage 1
+            {
+                $lookup: {
+                    "from": "",
+                    "localField": "",
+                    "foreignField": "",
+                    "as": ""
+                }
+            },
+            // Stage 2
+            {
+                $unwind: ""
+            },
+            //stage 3
+            {
+                $match: {
+                    "": data.location
+                }
+            },
+        ]
+        Plans.aggregate(pipeline, function (err, found) {
+            if (err || _.isEmpty(found)) {
+                callback(err, null);
+            } else {
+                callback(null, found)
+            }
+        });
+    },
+
+    checkCurrnecyArrived: function (data, callback) {
+
+    },
+
+    makeWithdrawal: function (data, callback) {
+
+    },
+
+    updateStatus: function (data, callback) {
+
+    }
+
+
+};
 module.exports = _.assign(module.exports, exports, model);
